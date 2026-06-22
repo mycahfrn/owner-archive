6b7e42b151e68e396a19cd93f1b94ed9267fbe89
mikael <mikaelfernandezj@gmail.com>
2026-06-21 21:18:39 -0400

    fixed site astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mycahfn.github.io/',
+  site: 'https://mycahfn.github.io',
   base:  "/owner-archive",
   vite: {
     plugins: [tailwindcss()]
