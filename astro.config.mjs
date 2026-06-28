ca85946c42a03149bbe6a2cf81c7f21cef587b68
mikael <mikaelfernandezj@gmail.com>
2026-06-27 22:54:23 -0400

    add responsive index

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import tunnel from 'astro-tunnel';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mycahfn.github.io',
   base:  "/owner-archive",
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [tunnel()],
 });
